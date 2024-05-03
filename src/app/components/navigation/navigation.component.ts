e9d0a6488d565df7f5e1aefa1ac1ce6b68fbebb3
Aleksei Taraskin <bakamoena@gmail.com>
2024-05-04 01:01:20 +0700

    loading spinner

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatDrawerContainer, MatSidenavModule } from "@angular/material/sidenav";
 import {ProjectListComponent} from "../project-list/project-list.component";
 import {ProjectTasksComponent} from "../project-tasks/project-tasks.component";
+import { ProjectDataService } from '../../services/project-data.service';
+import {MatProgressSpinner} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'app-navigation',
@@ -13,10 +15,11 @@ import {ProjectTasksComponent} from "../project-tasks/project-tasks.component";
     MatSidenavModule,
     ProjectListComponent,
     ProjectTasksComponent,
+    MatProgressSpinner,
   ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
-
+  public dataService = inject(ProjectDataService);
 }
